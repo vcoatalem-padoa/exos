632bd6815940894b10e790337fe44c4cf27d7242
Victor Coatalem <victor.c@padoa-group.com>
2025-06-05 17:50:42 +0200

    first exo

--- longest-consecutive-sequence/longest-consecutive-sequence.js
+++ longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -2,14 +2,15 @@
  * @method longestConsecutiveSequence
  * @param arr: an array of relative integers. e.g: [1, 5, -2, -1]
  * @returns the length of the longest sequence of sequential number that can be formed using numbers from the provided array, or array.length if array.length <= 1
- * 
- * ex: 
- * 
+ *
+ * ex:
+ *
  * longestConsecutiveSequence([1, 2, 3]) === 3
  * longestConsecutiveSequence([2, 4, 1]) === 2
  * longestConsecutiveSequence([-42, 0, 10, 1, -1]) === 2
- * 
+ *
  */
 export const longestConsecutiveSequence = (array) => {
-
-}
+  //FIXME
+  return -1;
+};
--- longest-consecutive-sequence/longest-consecutive-sequence.test.js
+++ longest-consecutive-sequence/longest-consecutive-sequence.test.js
@@ -1,3 +1,16 @@
+import { it, expect } from "vitest";
 import { longestConsecutiveSequence } from "./longest-consecutive-sequence";
 
-console.log("todo");
+it.each([
+  {
+    array: [1, 2, 3],
+    expected: 3,
+  },
+  {
+    array: [2, 4, 1],
+    expected: 2,
+  },
+  //TODO: complete test cases
+])("returns $expected when array is $array", ({ array, expected }) => {
+  expect(longestConsecutiveSequence(array)).toBe(expected);
+});
