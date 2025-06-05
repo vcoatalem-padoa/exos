a542c29c36099e6c25544108decbfb8b13166fe7
Victor Coatalem <victor.c@padoa-group.com>
2025-06-05 17:53:55 +0200

    update description

--- longest-consecutive-sequence/longest-consecutive-sequence.js
+++ longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -1,13 +1,16 @@
 /**
  * @method longestConsecutiveSequence
- * @param arr: an array of relative integers. e.g: [1, 5, -2, -1]
- * @returns the length of the longest sequence of sequential number that can be formed using numbers from the provided array, or array.length if array.length <= 1
+ * 
+ * @param array: an array of relative integers. e.g: [1, 5, -2, -1]
+ * 
+ * @returns the length of the longest sequence of sequential number that can be formed using numbers from the provided array
+ * 
+ * @description a sequence of sequential numbers is a sequence of numbers that can be ordered in a way that each number is followed by the next number in the sequence. We consider a sequence of 1 number to be valid.
+ * 
  *
- * ex:
- *
- * longestConsecutiveSequence([1, 2, 3]) === 3
- * longestConsecutiveSequence([2, 4, 1]) === 2
- * longestConsecutiveSequence([-42, 0, 10, 1, -1]) === 2
+ * @example longestConsecutiveSequence([1, 2, 3]) === 3
+ * @example longestConsecutiveSequence([2, 4, 1]) === 2
+ * @example longestConsecutiveSequence([-42, 0, 10, 1, -1]) === 2
  *
  */
 export const longestConsecutiveSequence = (array) => {
